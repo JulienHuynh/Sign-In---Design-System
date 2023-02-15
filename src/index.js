e01374f0fae7622776705e090c9ac8f1cc75113c
Julien Huynh <julienhuynh@MacBook-Pro-de-Julien.local>
2023-02-15 10:15:10 +0100

    added font & variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './pages/App';
 import './static/css/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/App.js
+++ src/pages/App.js
@@ -2,7 +2,6 @@ function App() {
   return (
     <div className="App">
         Hello world !!
-        <p className="title">title</p>
     </div>
   );
 }
