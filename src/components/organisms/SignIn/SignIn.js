46457e3e0563650e1d66bdc5bc2010924ab69435
Julien Huynh <julienhuynh@MacBook-Pro-de-Julien.local>
2023-02-16 16:21:37 +0100

    add Signin component

--- src/components/organisms/SignIn/SignIn.js
+++ src/components/organisms/SignIn/SignIn.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react'
 import Link from "../../atoms/Link/Link";
+import {FormInput} from "../../molecules/FormInput/FormInput";
 
-function SignIn (props) {
+export const Signin = ({ title, action, ...props }) => {
 
     return (
-        <>
-            <h1>Sign In</h1>
-            <Link linkClass="-regular" linkLabel="Crée un compte" setCurrentView={props.setCurrentView} view="SignUp" />
+        <div className="sign-form">
+            <h1>{title}</h1>
+            <form action={action} method='POST' className="form-section">
+                <FormInput {...props}></FormInput>
+                <FormInput {...props}></FormInput>
+            </form>
+            <Link linkClass="-regular" linkLabel="Créer un compte" setCurrentView={props.setCurrentView} view="SignUp" />
             <Link linkClass="-regular" linkLabel="Mot de passe oublié" setCurrentView={props.setCurrentView} view="UpdatePassword" />
-        </>
-    )
+        </div>
+    );
 }
 
-export default SignIn
+export default Signin;
