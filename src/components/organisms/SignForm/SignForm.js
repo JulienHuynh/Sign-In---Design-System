0cc4d67d7de1d0366601a0f3945146f211e3a111
Julien Huynh <julienhuynh@MacBook-Pro-de-Julien.local>
2023-02-16 14:54:37 +0100

    add sign organism

--- src/components/organisms/SignForm/SignForm.js
+++ src/components/organisms/SignForm/SignForm.js
@@ -4,11 +4,11 @@ import Link from "../../atoms/Link/Link";
 function SignIn (props) {
 
     return (
-        <>
+        <React.Fragment>
             <h1>Sign In</h1>
             <Link linkClass="-regular" linkLabel="Crée un compte" setCurrentView={props.setCurrentView} view="SignUp" />
             <Link linkClass="-regular" linkLabel="Mot de passe oublié" setCurrentView={props.setCurrentView} view="UpdatePassword" />
-        </>
+        </React.Fragment>
     )
 }
 
---
+++ src/components/organisms/SignForm/SignForm.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Sign } from './Input';
+
+export default {
+    component: Input,
+};
+
+const Template = (args) => <Input {...args} />;
+
+export const signRender = Template.bind({});
+signRender.args = {
+
+};
